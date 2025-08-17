a3dfc5a48cc4b93725fa765b9f911020d2213af2
Winecellar <winecellarcustomerservice@gmail.com>
2025-08-17 14:03:06 +0530

    Fix mobile menu links and navbar sizing

--- src/components/header/sub-components/MobileNavMenu.js
+++ src/components/header/sub-components/MobileNavMenu.js
@@ -4,35 +4,40 @@ import { useTranslation } from "react-i18next";
 const MobileNavMenu = () => {
   const { t } = useTranslation();
 
+  const closeMobileMenu = () => {
+    const offcanvas = document.querySelector('#offcanvas-mobile-menu');
+    if (offcanvas) offcanvas.classList.remove('active');
+  };
+
   return (
     <nav className="offcanvas-navigation" id="offcanvas-navigation">
       <ul>
         <li className="menu-item-has-children">
-          <Link to={process.env.PUBLIC_URL + "/"}>{t("home")}</Link>
+          <Link to={process.env.PUBLIC_URL + "/"} onClick={closeMobileMenu}>{t("home")}</Link>
         </li>
 
         <li className="menu-item-has-children">
-          <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
+          <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"} onClick={closeMobileMenu}>
             {t("shop")}
           </Link>
         </li>
         <li className="menu-item-has-children">
-          <Link to={process.env.PUBLIC_URL + "/"}>
+          <Link to={process.env.PUBLIC_URL + "/about-us"} onClick={closeMobileMenu}>
             {t("About Us")}
           </Link>
         </li>
         <li>
-          <Link to={process.env.PUBLIC_URL + "/"}>
+          <Link to={process.env.PUBLIC_URL + "/contact-us"} onClick={closeMobileMenu}>
             {t("Contact Us")}
           </Link>
         </li>
         <li>
-          <Link to={process.env.PUBLIC_URL + "/trade-customer"}>
+          <Link to={process.env.PUBLIC_URL + "/trade-customer"} onClick={closeMobileMenu}>
             TRADE CUSTOMERS CLICK HERE
           </Link>
         </li>
         <li>
-          <Link to={process.env.PUBLIC_URL + "/order-status"}>
+          <Link to={process.env.PUBLIC_URL + "/order-status"} onClick={closeMobileMenu}>
             TRACK ORDER
           </Link>
         </li>
